53c16233909a11cb9a2204d0dbe5fb01f1e07a70
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-04 00:37:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 def won?(board)
-  for each win_combo in WIN_COMBINATIONS
+  for each win_combo in WIN_COMBINATIONS do
     win_index_1 = win_combo[0]
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
